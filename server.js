67c60673149fd6e769f659556b5f17ebbc8b1b1e
Dylan Caldwell <dylan.caldwell35@gmail.com>
2024-03-21 19:42:28 -0400

    New vid. Better server logger

--- server.js
+++ server.js
@@ -9,13 +9,53 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/client'));
 
+const formatDate = date => {
+    const day = date.getDay();
+    let weekDay;
+    switch(day) {
+        case 0:
+            weekDay = 'Sun';
+            break;
+        case 1:
+            weekDay = 'Mon';
+            break;
+        case 2:
+            weekDay = 'Tue';
+            break;
+        case 3:
+            weekDay = 'Wed';
+            break;
+        case 4:
+            weekDay = 'Thu';
+            break;
+        case 5:
+            weekDay = 'Fri';
+            break;
+        case 6:
+            weekDay = 'Sat';
+            break;
+    }
+    let pm = false;
+    const hr = date.getHours();
+    let hrs;
+    if (hr > 12) {
+        hrs = hr - 12;
+        pm = true;
+    } else {
+        hrs = hr;
+    }
+    const time = `${hrs.toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}${pm ? 'pm' : 'am'}`;
+
+    return `[ ${weekDay} ${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()} ${time} ]`;
+}
+
 // Main route
 app.get('/', (req, res) => {
     // Logger
     const date = new Date();
-    const time = `[ ${date.getHours()}:${date.getMinutes()} ]`;
-    console.log(time, req.method, req.url, res.statusCode);
+    const time = formatDate(date);
     console.log('');
+    console.log(time, req.method, req.url, res.statusCode, req.headers['user-agent']);
 
     res.sendFile(__dirname + '/views/index.html');
 });
